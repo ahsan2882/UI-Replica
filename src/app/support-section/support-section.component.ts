6c683c8ad38021e80d720848921508b216476a8e
ahsan2882 <ahsanshahid2882@gmail.com>
2023-06-04 00:45:00 +0500

    animations for support section

--- src/app/support-section/support-section.component.ts
+++ src/app/support-section/support-section.component.ts
@@ -4,6 +4,9 @@ import {
   Input,
   OnChanges,
   SimpleChanges,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
 } from '@angular/core';
 
 @Component({
@@ -14,9 +17,46 @@ import {
 })
 export class SupportSectionComponent implements OnChanges {
   @Input() sectionAnimation: 'hidden' | 'visible' = 'hidden';
+  @ViewChild('heading') heading!: ElementRef<HTMLElement>;
+  @ViewChild('textElement') textElement!: ElementRef<HTMLDivElement>;
+  @ViewChild('buttonElement') buttonElement!: ElementRef<HTMLButtonElement>;
+  @ViewChild('imageElement') imageElement!: ElementRef<HTMLImageElement>;
+
+  constructor(private cdr: ChangeDetectorRef) {}
   ngOnChanges(changes: SimpleChanges): void {
-    if (!changes['sectionAnimation'].firstChange) {
-      console.log(changes['sectionAnimation'].currentValue);
+    const sectionAnimationChange = changes['sectionAnimation'];
+    if (sectionAnimationChange && !sectionAnimationChange.firstChange) {
+      if (
+        sectionAnimationChange.currentValue === 'visible' &&
+        sectionAnimationChange.previousValue === 'hidden'
+      ) {
+        const headingSpans = Array.from(
+          this.heading.nativeElement.children
+        ).filter((element) => element.tagName === 'SPAN');
+        headingSpans.forEach((element, index) => {
+          element.classList.remove(`text-appear-${index}`);
+        });
+        const textDivs = Array.from(
+          this.textElement.nativeElement.children
+        ).filter((element) => element.tagName === 'DIV');
+        textDivs.forEach((element, index) => {
+          element.classList.remove(`box-appear-${index}`);
+        });
+        this.buttonElement.nativeElement.classList.remove('button-appear');
+        this.imageElement.nativeElement.classList.remove('image-appear');
+        this.cdr.markForCheck();
+        setTimeout(() => {
+          headingSpans.forEach((element, index) => {
+            element.classList.add(`text-appear-${index}`);
+          });
+          textDivs.forEach((element, index) => {
+            element.classList.add(`box-appear-${index}`);
+          });
+          this.buttonElement.nativeElement.classList.add('button-appear');
+          this.imageElement.nativeElement.classList.add('image-appear');
+          this.cdr.markForCheck();
+        }, 10);
+      }
     }
   }
 }
