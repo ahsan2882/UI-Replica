e985ae9f60d08e24a3b22a0bd5f8f3abf705dc05
ahsan2882 <ahsanshahid2882@gmail.com>
2023-06-04 14:06:52 +0500

    animations  for footer section

--- src/app/footer-section/footer/footer.component.ts
+++ src/app/footer-section/footer/footer.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  ChangeDetectorRef,
+  ViewChild,
+  OnChanges,
+  SimpleChanges,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +15,35 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   styleUrls: ['./footer.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {
+export class FooterComponent implements OnChanges {
   @Input() triggerAnimation: 'hidden' | 'visible' = 'hidden';
+  @ViewChild('logo') logo!: ElementRef<HTMLElement>;
+  @ViewChild('name') name!: ElementRef<HTMLElement>;
+  @ViewChild('link') link!: ElementRef<HTMLElement>;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const triggerAnimationChange = changes['triggerAnimation'];
+    if (triggerAnimationChange && !triggerAnimationChange.firstChange) {
+      if (
+        triggerAnimationChange.currentValue === 'visible' &&
+        triggerAnimationChange.previousValue === 'hidden' &&
+        this.logo &&
+        this.name &&
+        this.link
+      ) {
+        this.logo.nativeElement.classList.remove('logo-appear');
+        this.name.nativeElement.classList.remove('name-appear');
+        this.link.nativeElement.classList.remove('link-appear');
+        this.cdr.markForCheck();
+        setTimeout(() => {
+          this.logo.nativeElement.classList.add('logo-appear');
+          this.name.nativeElement.classList.add('name-appear');
+          this.link.nativeElement.classList.add('link-appear');
+          this.cdr.markForCheck();
+        }, 10);
+      }
+    }
+  }
 }
